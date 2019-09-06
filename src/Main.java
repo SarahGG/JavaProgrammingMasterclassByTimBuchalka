ee37e8af04e32257bb4077bb7e81866d7a7d75fc
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-06 10:02:03 -0400

    adding variable int mySecondNumber

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
         System.out.println("Hello World!");
 
         // Chapter 3, Lesson 22
-        int myFirstNumber = 4;
+        int myFirstNumber = 4 + (10 + 2);
         System.out.println("myFirstNumber is " + myFirstNumber);
+
+        // Chapter 3, Lesson 23
+        int mySecondNumber = 7 % 2;
     }
 }
