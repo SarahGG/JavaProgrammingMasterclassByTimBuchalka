d1c72c31b0308b9302e96c54e447d967aae27c58
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-06 09:55:41 -0400

    Added int "myFirstNumber"
    I really can't wait to be through the first chapters :)

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         // Chapter 3, Lesson 19
         System.out.println("Hello World!");
+
+        // Chapter 3, Lesson 22
+        int myFirstNumber = 4;
     }
 }
