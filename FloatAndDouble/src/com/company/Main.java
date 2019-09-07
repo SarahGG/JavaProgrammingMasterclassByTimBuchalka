fce12182b04cb95afeb8714917b2484d75b435b2
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-06 20:22:50 -0400

    Added myMaxFloatValue variable

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    // Get the MIN and MAX value of floats and doubles
+	// Get the MIN and MAX value of floats and doubles
         float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
     }
 }
