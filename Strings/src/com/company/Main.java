64dd4c906d8221f9bfb08c5c740928c47e7b07ea
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-12 15:00:37 -0400

    Completed lesson 31

---
+++ Strings/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myOrder = "We'll have 2 dinners, please. ";
+        System.out.println(myOrder);
+
+        myOrder += "Oh! And dessert!";
+        System.out.println(myOrder);
+
+        System.out.println("My Copyright \u00A9 2019");
+
+        String stringNum1 = "10";
+        String stringNum2 = "20";
+        System.out.println(stringNum1 + stringNum2);
+    }
+}
