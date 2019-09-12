6b99b4c98bcf391f388c5768e344b1aee742e00e
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-12 11:09:53 -0400

    Completed lesson 30

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+
+        System.out.printf("Using unicode, %c should be the same as %c%n", myChar, myUnicodeChar);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+        boolean isTodayRainy = true;
     }
 }
