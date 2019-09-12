608da871028f672ac2f2f0e8f682b6102a72c226
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-11 23:46:45 -0400

    Finishing lesson 28

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -12,5 +12,10 @@ public class Main {
 
         System.out.printf("The Float value range is from %f to %f%n", myMinFloatValue, myMaxFloatValue);
         System.out.printf("The Double value range is from %e to %e%n", myMinDoubleValue, myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float mtFloatValue = 5.25f;
+        double myDoubleValue 5.25d;
+
     }
 }
