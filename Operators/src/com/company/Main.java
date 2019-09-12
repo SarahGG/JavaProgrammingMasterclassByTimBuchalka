d1beed1e787e3f68b83cdee854da418cf02a9054
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-12 15:38:26 -0400

    Completed lesson 33

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -21,5 +21,24 @@ public class Main {
 
         result = result % 3;
         System.out.println("4 % 3 = " + result);
+
+        // lesson 33 starts here
+        result++;
+        System.out.println("1 + 1 = " + result);
+
+        result--;
+        System.out.println("2 - 1 = " + result);
+
+        result += 2;
+        System.out.println("1 + 2 = " + result);
+
+        result *= 10;
+        System.out.println("3 * 10 = " + result);
+
+        result /= 3;
+        System.out.println("30 / 3 = " + result);
+
+        result -= 2;
+        System.out.println("10 - 2 = " + result);
     }
 }
