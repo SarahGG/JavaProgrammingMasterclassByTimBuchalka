6f368a54a10305228d7bd85a25a0b315e444e95b
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-12 00:10:00 -0400

    completed chapter 29 mini challenge

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// Get the MIN and MAX value of floats and doubles
+        Scanner scanner = new Scanner(System.in);
+
+	    // Get the MIN and MAX value of floats and doubles
         float myMinFloatValue = Float.MIN_VALUE;
         float myMaxFloatValue = Float.MAX_VALUE;
-
         double myMinDoubleValue = Double.MIN_VALUE;
         double myMaxDoubleValue = Double.MAX_VALUE;
-
         System.out.printf("The Float value range is from %f to %f%n", myMinFloatValue, myMaxFloatValue);
         System.out.printf("The Double value range is from %e to %e%n", myMinDoubleValue, myMaxDoubleValue);
 
@@ -18,13 +20,18 @@ public class Main {
         int myIntValue = 5 / 2;
         float myFloatValue = 5f / 2f;
         double myDoubleValue = 5d / 2d;
-
         System.out.printf("My Int Value: %d%n" +
                 "my Float Value: %f%n" +
                 "My Double Value: %f%n",
                 myIntValue, myFloatValue, myDoubleValue);
 
         System.out.print("\n");
+
+        System.out.print("Enter weight in pounds: ");
+        int weightInPounds = scanner.nextInt();
+        double kilogramPer1Pound = 0.45359237d;
+        double weightInKilograms = weightInPounds * kilogramPer1Pound;
+        System.out.printf("Weight is %f kilograms", (weightInKilograms));
 
     }
 }
