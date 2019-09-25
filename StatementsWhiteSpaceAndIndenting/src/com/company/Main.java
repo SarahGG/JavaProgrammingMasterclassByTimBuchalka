8f64a7f4fe473ccd1b9e02a6943c19ae2c39e6ed
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-25 12:48:17 -0400

    Completed lesson 44

---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int myVariable=50;
+	myVariable++;
+	myVariable--;
+
+        System.out.println("This i a test");
+        System.out.println("This is" +
+                " another" +
+                " and another.");
+
+        int myInt = 5; int anotherInt = 10;
+    }
+}
