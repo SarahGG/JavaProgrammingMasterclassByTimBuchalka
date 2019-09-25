54170f8f3eac00ef7d3a01d3641ca0a1e55e5e63
Sarah Guarino <guarino.sgg@gmail.com>
2019-09-25 13:47:25 -0400

    Completed lesson 48 and 49

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         int finalScore = calculateScore(true, 800,5, 100);
-        System.out.println("Your final score was " + finalScore);
         finalScore = calculateScore(true, 10000, 8, 200);
-        System.out.println("Your final score was " + finalScore);
+
+        displayHighScorePosition("Henry", calculateHighScorePosition(1500));
+        displayHighScorePosition("Samantha", calculateHighScorePosition(900));
+        displayHighScorePosition("Rami", calculateHighScorePosition(400));
+        displayHighScorePosition("Kiko", calculateHighScorePosition(50));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -17,6 +20,22 @@ public class Main {
             return finalScore;
         } else {
             return -1;
+        }
+    }
+
+    public static void displayHighScorePosition(String playerName, int tablePosition) {
+        System.out.println(playerName + " is in the " + tablePosition + " position");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if(playerScore >= 1000) {
+            return 1;
+        } else if(playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
         }
     }
 }
