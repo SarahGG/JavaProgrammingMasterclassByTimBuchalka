3c247428edd1e368f60310002f2bfefd266a6d7f
Sarah Guarino <guarino.sgg@gmail.com>
2019-10-21 13:22:38 -0400

    Adding the dividLBYL method to the Main class

--- Section 14/Exceptions/src/com/company/Main.java
+++ Section 14/Exceptions/src/com/company/Main.java
@@ -5,4 +5,12 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
     }
+
+    private static int divideLBYL (int x, int y) {
+        if(y != 0) {
+            return x / y;
+        } else {
+            return 0;
+        }
+    }
 }
