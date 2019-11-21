55828757f48daf88caa300d2e028559518270443
Sarah Guarino <guarino.sgg@gmail.com>
2019-11-21 13:28:23 -0500

    Completed Chapter 14 Challenge 2

--- Section 17/RegularExpressions/src/com/company/Main.java
+++ Section 17/RegularExpressions/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -15,8 +16,22 @@ public class Main {
         System.out.println("\t\tOur regex matches: " + Pattern.matches("^I want a bike.$", challenge1String));
     }
 
+    private static void challenge2() {
+        buildMenu("Challenge 2",
+                "\tNow write a regular expression that will match \"I want a bike.\" and \"I want a ball.\".\n" +
+                        "\tVerify your answer using the matches() method.");
+
+        String ch2regexPattern = "^I want a (bike|ball).$";
+        System.out.println("\t\tregex pattern: " + ch2regexPattern);
+        String ch2StringBall = "I want a ball.";
+        System.out.println("\t\tRegex matches \"" + ch2StringBall + "\": " + Pattern.matches(ch2regexPattern, ch2StringBall));
+        String ch2StringBike = "I want a bike.";
+        System.out.println("\t\tRegex matches \"" + ch2StringBike + "\": " + Pattern.matches(ch2regexPattern, ch2StringBike));
+    }
+
     public static void main(String[] args) {
-        challenge1();l
+        challenge1();
+        challenge2();
     }
 
     private static void buildMenu(String menuMessage, String challengeMessage) {
