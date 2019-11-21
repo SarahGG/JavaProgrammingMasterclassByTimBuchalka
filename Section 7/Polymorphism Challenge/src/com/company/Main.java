6bac8c0232c68e7b31b3c7f3e2313c1c5040a0b9
Sarah Guarino <guarino.sgg@gmail.com>
2019-11-21 14:43:49 -0500

    Added program challenge notes

--- Section 7/Polymorphism Challenge/src/com/company/Main.java
+++ Section 7/Polymorphism Challenge/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Create a base class called Car.
+        // It should hae a few fields that would be appropriate for a generic car class, like engine, wheels, etc.
+        // Make sure that some parameters need to be passed upon initialization.
+        // Create getters and setters.
+        // create some methods like start, accelerate, and brake.
+        // Now, make 3 classes that extend Car and demonstrate polymorphism.
+
+
     }
 }
