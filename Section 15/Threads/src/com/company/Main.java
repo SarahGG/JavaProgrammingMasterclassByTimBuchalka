d555e2d06eac699852cb9b8ef882eb69bb532395
Sarah Guarino <guarino.sgg@gmail.com>
2019-11-22 15:35:39 -0500

    Adding two basic thread executions

--- Section 15/Threads/src/com/company/Main.java
+++ Section 15/Threads/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import static com.company.ThreadColor.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // you can extend Thread
+        Thread anotherThread = new AnotherThread(){
+            @Override
+            public void run() {
+                System.out.println(ANSI_GREEN + "Hello from Anonymous method");
+            }
+        };
+        anotherThread.start();
+
+        // you can also implement Runnable
+        Thread myRunnableThread = new Thread(new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ANSI_CYAN + "Hello from my anonymous class's implementation of MyRunnable.");
+            }
+        });
+        myRunnableThread.start();
+
+
     }
 }
